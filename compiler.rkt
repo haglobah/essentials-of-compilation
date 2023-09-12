72fcb2bf8ba8bb5b6b8328e50b7cd289b72ea533
Beat Hagenlocher <bah@posteo.de>
2023-09-12 21:29:37 +0200

    Add first exercises

--- compiler.rkt
+++ compiler.rkt
@@ -39,17 +39,33 @@
     [((Int n1) (Int n2)) (Int (fx+ n1 n2))]
     [(_ _) (Prim '+ (list r1 r2))]))
 
+(define (pe-sub r1 r2)
+  (match* (r1 r2)
+    [((Int n) (Int m)) (Int (fx- n m))]
+    [(_ _) (Prim '- (list r1 r2))]))
+   
 (define (pe-exp e)
   (match e
     [(Int n) (Int n)]
     [(Prim 'read '()) (Prim 'read '())]
     [(Prim '- (list e1)) (pe-neg (pe-exp e1))]
-    [(Prim '+ (list e1 e2)) (pe-add (pe-exp e1) (pe-exp e2))]))
+    [(Prim '+ (list e1 e2)) (pe-add (pe-exp e1) (pe-exp e2))]
+    [(Prim '- (list e1 e2)) (pe-sub (pe-exp e1) (pe-exp e2))]))
 
 (define (pe-Lint p)
   (match p
     [(Program info e) (Program info (pe-exp e))]))
 
+(define (test-partial-expansion p)
+  (assert "testing pe-Lint"
+          (equal? (interp-Lint p) (interp-Lint (pe-Lint p)))))
+(define (test-pe p)
+  #;(print `(program () ,p))
+  (test-partial-expansion (parse-program `(program () ,p))))
+
+(test-pe '(+ 10 (- (+ 5 3) (+ 2 2))))
+(test-pe '(+ 3 1))
+(test-pe '(+ 1 1))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; HW1 Passes
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
